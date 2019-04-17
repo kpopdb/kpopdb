483cbbf648aac81f866050c5488b8db5cdc36b4d
Alexander Dang-Tran <alex.dangtran@gmail.com>
2019-04-17 07:53:50 +0200

    [idol] Add Lee Saerom
    Add initial idol schema and first idol.

--- validate.py
+++ validate.py
@@ -18,7 +18,21 @@ _validator_class = validator_for(GROUP_SCHEMA)
 _validator_class.check_schema(GROUP_SCHEMA)
 group = _validator_class(GROUP_SCHEMA)
 
-with open(os.path.join("gg", "fromis_9", "fromis_9.group.json")) as f:
+with open("idol.schema.json") as f:
+    IDOL_SCHEMA = json.load(f)
+_validator_class = validator_for(IDOL_SCHEMA)
+_validator_class.check_schema(IDOL_SCHEMA)
+idol = _validator_class(IDOL_SCHEMA)
+
+file_path = os.path.join("gg", "fromis_9", "fromis_9.group.json")
+with open(file_path, 'r') as f:
     data = json.load(f)
 
+print("Validating {}".format(file_path))
 group.validate(data)
+
+file_path = os.path.join("gg", "fromis_9", "lee_saerom.idol.json")
+with open(file_path, 'r') as f:
+    data = json.load(f)
+print("Validating {}".format(file_path))
+idol.validate(data)
