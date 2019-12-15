6db471591309cbe15c9a53d049e7a22814d400ee
Alexander Dang-Tran <alex.dangtran@gmail.com>
2019-12-14 22:15:04 -0600

    Refactor validation into a class
    Add additional checks for consistency between group and idol data

--- validate.py
+++ validate.py
@@ -12,27 +12,67 @@ import jsonschema
 
 from jsonschema.validators import validator_for
 
+class KpopValidator(object):
+    def __init__(self, base_folder):
+        self.base_path = os.path.abspath(base_folder)
+
+        with open(os.path.join(self.base_path, "group.schema.json")) as f:
+            group_schema = json.load(f)
+        _validator_class = validator_for(group_schema)
+        _validator_class.check_schema(group_schema)
+        self.v_group = _validator_class(group_schema)
+
+        with open(os.path.join(self.base_path, "idol.schema.json")) as f:
+            idol_schema = json.load(f)
+        _validator_class = validator_for(idol_schema)
+        _validator_class.check_schema(idol_schema)
+        self.v_idol = _validator_class(idol_schema, format_checker=jsonschema.FormatChecker())
+
+    def _validate_group(self, folder):
+        """Validate a group's kpop data
+
+        Criteria:
+         - There should be one .group.json file.
+         - folder name should match .group.json file name.
+         - Group data fits group schema
+         - All .idol.json files underneath should match the group.
+         - All idol data fits idol schema
+        """
+        print("Validating {}".format(folder))
+        group_list = glob.glob(os.path.join(folder, '*.group.json'))
+
+        # Only one .group.json file
+        if len(group_list) != 1:
+            raise RuntimeError(f"Too many .group files in {folder}: {group_list}")
+        group = group_list[0]
+        with open(group, 'r') as f:
+            group_data = json.load(f)
+        # Validate against schema
+        self.v_group.validate(group_data)
+        # Save off group data for idol data consistency
+        group_hangul = group_data['nameHangul']
+        group_roman = group_data['nameRoman']
+
+        idol_list = glob.iglob(os.path.join(folder, '*.idol.json'))
+        for idol in idol_list:
+            with open(idol, 'r') as f:
+                idol_data = json.load(f)
+            # Validate against schema
+            self.v_idol.validate(idol_data)
+            # Idol data should have consistent group data
+            if ((idol_data['groupHangul'] != group_hangul) or
+                (idol_data['groupRoman'] != group_roman)):
+                raise RuntimeError(f"Idol group data for {idol_data['stageNameRoman']} does not match group name: {group_hangul} {group_roman}")
+
+        return
+
+    def validate(self):
+        """Validate all data in base_path
+        """
+        for path in glob.iglob(os.path.join(self.base_path, '**/*.group.json'), recursive=True):
+            folder = os.path.split(path)[0]
+            self._validate_group(folder)
+
 if __name__ == '__main__':
-    with open("group.schema.json") as f:
-        GROUP_SCHEMA = json.load(f)
-    _validator_class = validator_for(GROUP_SCHEMA)
-    _validator_class.check_schema(GROUP_SCHEMA)
-    group = _validator_class(GROUP_SCHEMA)
-
-    with open("idol.schema.json") as f:
-        IDOL_SCHEMA = json.load(f)
-    _validator_class = validator_for(IDOL_SCHEMA)
-    _validator_class.check_schema(IDOL_SCHEMA)
-    idol = _validator_class(IDOL_SCHEMA, format_checker=jsonschema.FormatChecker())
-
-    for file_path in glob.iglob('./**/*.group.json', recursive=True):
-        print("Validating {}".format(file_path))
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-        group.validate(data)
-
-    for file_path in glob.iglob('./**/*.idol.json', recursive=True):
-        print("Validating {}".format(file_path))
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-        idol.validate(data)
+    v = KpopValidator(os.path.split(os.path.abspath(__file__))[0])
+    v.validate()
